4219f57461358d28aa1916003492fccca7935476
exxedridel <exxed.ridel@gmail.com>
2023-05-24 19:32:56 -0600

    ADD router and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import TasksPage from "./pages/TasksPage";
+import TaskForm from "./pages/TaskForm";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Heved</h1>
-      <div className="card">
-        <p>Choo Choo! This is an example of a Vite + React app running on Railway.</p>
-      </div>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<TasksPage />} />
+      <Route path="/new" element={<TaskForm />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
