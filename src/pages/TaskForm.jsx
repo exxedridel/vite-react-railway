6c1c19fd4e7bdeee0fcce87bb1e4cede56177e59
exxedridel <exxed.ridel@gmail.com>
2023-05-24 20:19:15 -0600

    ADD create task FE functionality

--- src/pages/TaskForm.jsx
+++ src/pages/TaskForm.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { Form, Formik } from "formik";
 
 function TaskForm() {
   return (
-    <div>TaskForm</div>
-  )
+    <div>
+      <Formik
+        initialValues={{
+          title: "",
+          description: "",
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ handleChange, handleSubmit }) => (
+          <Form onSubmit={handleSubmit}>
+            <label htmlFor="title">Title</label>
+            <input
+              id="title"
+              name="title"
+              type="text"
+              placeholder="Task title"
+              onChange={handleChange}
+            />
+
+            <label htmlFor="description">Description</label>
+            <textarea
+              id="description"
+              name="description"
+              rows="3"
+              placeholder="Describe the task"
+              onChange={handleChange}
+            />
+
+            <button type="submit">Save</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
-export default TaskForm
+export default TaskForm;
