7d50c62cdf78199a3ae6a34d12cc1c6139100d8f
exxedridel <exxed.ridel@gmail.com>
2023-05-25 23:50:12 -0600

    ADD get all employees from api and render them on FE functionality

--- src/api/tasks.api.js
+++ src/api/tasks.api.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+export const getEmployeesRequest = async (employee) =>
+  await axios.get(
+    "https://mysql-restapi-production-5f4b.up.railway.app/api/employees"
+  );
+
 export const createEmployeeRequest = async (employee) =>
   await axios.post(
     "https://mysql-restapi-production-5f4b.up.railway.app/api/employees",
--- src/pages/EmployeesPage.jsx
+++ src/pages/EmployeesPage.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { getEmployeesRequest } from "../api/tasks.api";
+import EmployeeCard from "../components/EmployeeCard";
 
 function EmployeesPage() {
-  return <div>Employees Page</div>;
+  const [employees, setEmployees] = useState([]);
+  useEffect(() => {
+    async function loadEmployees() {
+      const response = await getEmployeesRequest();
+      setEmployees(response.data);
+    }
+    loadEmployees();
+  }, []);
+
+  return (
+    <div>
+      <h1>Employees</h1>
+      {employees.map((employee) => (
+        <EmployeeCard employee={employee} key={employee.id} />
+      ))}
+    </div>
+  );
 }
 
 export default EmployeesPage;
