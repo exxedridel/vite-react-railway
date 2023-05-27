53de2c15e599e7d676f40db214e2db8d6b814cdc
exxedridel <exxed.ridel@gmail.com>
2023-05-27 00:45:46 -0600

    ADD tailwindcss and styles to entire app

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div>
+      <h2 className="text-3xl text-white font-bold uppercase text-center pt-20">404 Not Found</h2>
+      <p className="text-3xl text-white font-bold text-center py-5">Wrong route!</p>
+    </div>
   )
 }
 
