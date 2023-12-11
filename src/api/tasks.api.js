42da1787f982b884b201b25d1a8b9df8ee2deb85
hevedrios <hrios@tiprotec.com>
2023-12-11 02:02:56 -0600

    Add: .env file and their modifications for dinamic environment

--- src/api/tasks.api.js
+++ src/api/tasks.api.js
@@ -1,24 +1,23 @@
 import axios from "axios";
 
-const TASKS_ENDPOINT =
-  "https://mysql-restapi-production-5f4b.up.railway.app/api/tasks/";
+const baseUrl = import.meta.env.VITE_BASE_MIDDLEWARE;
+const path = "/api/tasks/";
 
-export const getTasksRequest = async () => 
-  await axios.get(TASKS_ENDPOINT);
+export const getTasksRequest = async () => await axios.get(baseUrl + path);
 
 export const createTaskRequest = async (task) =>
-  await axios.post(TASKS_ENDPOINT, task);
+  await axios.post(baseUrl + path, task);
 
 export const deleteTaskRequest = async (id) =>
-  await axios.delete(TASKS_ENDPOINT+id);
+  await axios.delete(baseUrl + path + id);
 
 export const getTaskRequest = async (id) =>
-  await axios.get(TASKS_ENDPOINT+id);
+  await axios.get(baseUrl + path + id);
 
 export const updateTaskRequest = async (id, updatedTask) =>
-  await axios.put(TASKS_ENDPOINT+id, updatedTask);
+  await axios.put(baseUrl + path + id, updatedTask);
 
 export const toggleTaskDoneRequest = async (id, isDone) =>
-  await axios.put(TASKS_ENDPOINT+id, {
+  await axios.put(baseUrl + path + id, {
     isDone,
   });
