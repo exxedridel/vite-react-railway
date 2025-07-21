f6110e94f600e4ab731030457c4500510bfad905
dev_hrios <hrios@tiprotec.com>
2025-07-21 15:10:07 -0600

    Update: Upgrade background, card, loader styles

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,6 +1,18 @@
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 const Loader = () => {
+  const [brand, setBrand] = useState("#000");
+  const [background, setBackground] = useState("#fff");
+
+  useEffect(() => {
+    const root = getComputedStyle(document.documentElement);
+    const brandHsl = root.getPropertyValue("--brand").trim();
+    const backgroundHsl = root.getPropertyValue("--background").trim();
+    setBrand(`hsl(${brandHsl})`);
+    setBackground(`hsl(${backgroundHsl})`);
+  }, []);
+
   return (
     <motion.ul className="flex justify-center items-center gap-2 h-4 w-20">
       {[...Array(3)].map((_, index) => (
@@ -11,11 +23,13 @@ const Loader = () => {
             width: [16, 4, 16],
             height: [16, 4, 16],
             opacity: [0.25, 1, 0.25],
-            backgroundColor: [
-              "rgb(116 180 236)",
-              "rgb(255 255 255)",
-              "rgb(116 180 236)",
-            ],
+            // elegir manualmente
+            // backgroundColor: [
+            //   "rgb(116 180 236)",
+            //   "rgb(255 255 255)",
+            //   "rgb(116 180 236)",
+            // ],
+            backgroundColor: [brand, background, brand],
           }}
           transition={{
             repeat: Infinity,
